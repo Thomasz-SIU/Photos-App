2cb8b1158b3f6e27730b42144b72d8c65dc0a4a6
thomasz <thomas.zemen@siu.edu>
2024-11-03 22:26:17 -0600

    delete photo by unique id

--- photoz-clone/src/main/java/com/jetbrains/thomas/photoz/clone/PhotozController.java
+++ photoz-clone/src/main/java/com/jetbrains/thomas/photoz/clone/PhotozController.java
@@ -1,14 +1,18 @@
 package com.jetbrains.thomas.photoz.clone;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class PhotozController {
 
-    List<Photo> photoList = List.of( new Photo("1", "photo1.jpg"));
+    HashMap<String, Photo> photoMap = new HashMap<>(){{put("1", new Photo("1", "photo1.jpg"));}};
     @GetMapping("/")
     public String HelloWorld()
     {
@@ -16,8 +20,24 @@ public class PhotozController {
     }
 
     @GetMapping("/photolist")
-    public List Photos()
+    public Collection<Photo> get()
     {
-        return photoList;
+        return photoMap.values();
+    }
+
+    @GetMapping("/photolist/{id}")
+    public Photo get(@PathVariable String id)
+    {
+        Photo photo = photoMap.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photolist/{id}")
+    public void removePhoto(@PathVariable String id)
+    {
+        Photo photo = photoMap.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        photoMap.remove(id);
     }
 }
